7361c4957273c9d425680d83276acc8a4bafedf3
Nour El Yakine ICHE <niche@asptt.com>
2021-01-19 17:15:08 +0100

    init

---
+++ src/Machine.java
@@ -0,0 +1,55 @@
+import enums.EDirection;
+import enums.EInstruction;
+import utils.Position;
+
+public class Machine {
+
+    Position position;
+    EDirection direction;
+    Maze maze;
+
+    public Machine(Position position, EDirection direction) {
+        this.position = position;
+        this.direction = direction;
+    }
+
+    public void instruct(EInstruction instruction) {
+
+        switch (instruction) {
+            case D:
+                setDirection(this.direction.next());
+                break;
+            case G:
+                setDirection(this.direction.prev());
+                break;
+            case A:
+                move();
+                break;
+        }
+
+    }
+
+    private void move() {
+
+        int newX = position.getX() + direction.getNextPOI().getX();
+        int newY = position.getY() + direction.getNextPOI().getY();
+        boolean canMove = maze.canMove(newX, newY);
+
+        if (canMove) {
+            this.position = new Position(newX, newY);
+        }
+    }
+
+
+    private void setDirection(EDirection direction) {
+        this.direction = direction;
+    }
+
+    public void setMaze(Maze maze) {
+        this.maze = maze;
+    }
+
+    public void printPosition() {
+        System.out.println("x=" + position.getX() + ", y=" + position.getY() + ", orientation=" + direction);
+    }
+}
---
+++ src/enums/EInstruction.java
@@ -0,0 +1,7 @@
+package enums;
+
+public enum EInstruction {
+    D, // pivoter à droite à 90
+    G, // pivoter à gauche à 90
+    A // move on case
+}
